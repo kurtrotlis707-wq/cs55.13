ac1ddd4fc8b0a6f3f716c883612b9702c9612e3a
Kurt R <krotlis707@gmail.com>
2025-09-03 08:59:05 -0700

    clonewk2

---
+++ week2/index.js
@@ -0,0 +1,31 @@
+let myhttp = require("http");
+
+
+let myserver = myhttp.createServer(
+
+    function (myrequest, myresponse) {
+        console.log(myrequest.url);
+
+        let mytext;
+        if (myrequest.url === '/hey') {
+            mytext = "hello ethere";
+        } else {
+            mytext = "Nope";
+        }
+
+
+
+
+
+
+
+
+
+
+        myresponse.writeHead(200, { "Content-Type": "text/plain" });
+
+        myresponse.end(mytext + "\n");
+    }
+);
+
+myserver.listen(8080, "127.0.0.1");
