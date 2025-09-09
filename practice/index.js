7bac08edf9c8bc4dc8a6f05f622b1b3ebe930b29
Kurt R <krotlis707@gmail.com>
2025-09-09 14:22:56 -0700

    wkagain

--- practice/index.js
+++ practice/index.js
@@ -1,50 +1,48 @@
+// Import the built-in HTTP module to create a web server
 const myhttp = require("http");
 
+// Import the file system module with promises for asynchronous file operations
 const fs = require("fs").promises;
 
-// let myserver = myhttp.createServer( {
-
-// function( myrequest, myresponse ) {
+// Define the request listener function that handles incoming HTTP requests
 const requestListener = function (myrequest, myresponse) {
+    // Log the requested URL to the console for debugging
     console.log(myrequest.url);
 
-    // console.log( myrequest.url );
-
-    // let mytext;
+    // Check if the request is for the root path '/'
     if (myrequest.url === '/') {
+        // Read the HTML file asynchronously
         fs.readFile(__dirname + "/page.html")
-
             .then(
                 contents => {
+                    // Set the response header to indicate HTML content with UTF-8 encoding
                     myresponse.setHeader("Content-Type", "text/html; charset=UTF-8");
 
+                    // Set the HTTP status code to 200 (OK)
                     myresponse.writeHead(200);
 
+                    // Send the HTML content as the response body
                     myresponse.end(contents);
                 });
     } else {
-
+        // For any other URL, read the JSON data file
         fs.readFile(__dirname + "/data.json")
-
             .then(
-
                 contents => {
-
+                    // Set the response header to indicate JSON content with UTF-8 encoding
                     myresponse.setHeader("Content-Type", "application/json; charset=UTF-8");
 
+                    // Set the HTTP status code to 200 (OK)
                     myresponse.writeHead(200);
 
+                    // Send the JSON content as the response body
                     myresponse.end(contents);
                 });
     }
 };
-let myserver = myhttp.createServer(
 
-    requestListener
-);
+// Create an HTTP server instance using the request listener function
+let myserver = myhttp.createServer(requestListener);
 
+// Start the server listening on localhost (127.0.0.1) port 8080
 myserver.listen(8080, "127.0.0.1");
-
-
-
-
--- week1/index.js
+++ week1/index.js
@@ -1,31 +1,33 @@
+// Import the built-in HTTP module to create a web server
 let myhttp = require("http");
 
-
+// Create an HTTP server with a request handler function
 let myserver = myhttp.createServer(
-
+    // Request handler function that processes incoming HTTP requests
     function (myrequest, myresponse) {
+        // Log the requested URL to the console for debugging purposes
         console.log(myrequest.url);
 
+        // Initialize variable to store response text
         let mytext;
+
+        // Check if the request URL is '/hey'
         if (myrequest.url === '/hey') {
+            // Set response text for the '/hey' route
             mytext = "hello ethere";
         } else {
+            // Set default response text for all other routes
             mytext = "Nope";
         }
 
-
-
-
-
-
-
-
-
-
+        // Set HTTP response headers
+        // 200 = OK status code, Content-Type specifies plain text response
         myresponse.writeHead(200, { "Content-Type": "text/plain" });
 
+        // Send the response text and end the response
         myresponse.end(mytext + "\n");
     }
 );
 
+// Start the server listening on localhost (127.0.0.1) port 8080
 myserver.listen(8080, "127.0.0.1");
